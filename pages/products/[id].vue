ca17c69278d96caf58266f1b042b3458729733cd
faustino21 <faustinoaron21@gmail.com>
2023-02-07 21:52:14 +0700

    add route parameter on folder products

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Product {{ id }}</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero facilis eaque voluptate quisquam labore, tenetur harum nisi quibusdam? Esse, rerum?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
