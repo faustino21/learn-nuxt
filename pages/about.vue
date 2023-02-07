1febc230fad05c8b89d08155ffd197ec250f2ed2
faustino21 <faustinoaron21@gmail.com>
2023-02-07 21:37:55 +0700

    adding pages

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nobis dicta vero atque eum, eligendi illo quas suscipit, animi, ipsum itaque assumenda.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
