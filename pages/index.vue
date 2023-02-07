dc18405e5a9c46e31cf141b29678bf3625fbe03b
faustino21 <faustinoaron21@gmail.com>
2023-02-07 22:11:40 +0700

    add nuxtlink componenst

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Tinos NuxtJs</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">about</NuxtLink></li>
+                    <li><NuxtLink to="/products">products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nobis dicta vero atque eum, eligendi illo quas suscipit, animi, ipsum itaque assumenda.</p>
     </div>
