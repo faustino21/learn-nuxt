c97a3f76d136e13f282b032134b163131febd57b
faustino21 <faustinoaron21@gmail.com>
2023-02-08 20:37:56 +0700

    add metadata and using usehead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     telemetry: false,
-    modules: [
-        '@nuxtjs/tailwindcss'
-      ]
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+      head : {
+        title: 'Nuxt App Tino',
+        meta : [
+          { name : 'description', content: 'Everythig About Nuxt 3'}
+        ],
+        link: [
+          {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+        ]
+      }
+    }
 })
