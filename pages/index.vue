7db3b5e4754844147e5ad5972219c9fbd4ea8357
faustino21 <faustinoaron21@gmail.com>
2023-02-09 08:54:36 +0700

    fetching to strapi

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,12 @@
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nobis dicta vero atque eum, eligendi illo quas suscipit, animi, ipsum itaque assumenda.</p>
     </div>
+    <div>project : {{ project }}</div>
+    <div>project Meta : {{ projectMeta }}</div>
 </template>
 
 <script setup>
-
+    const { data: project, meta: projectMeta } = await useFetch('http://localhost:1337/api/projects')
 </script>
 
 <style scoped>
